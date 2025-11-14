dc18b7505f74b4276ab260faebafc1b3665b28ef
Yusufcan Yar <yusufcan.yar@outlook.be>
2025-11-14 10:11:22 +0100

    Nog beginnen

---
+++ loop/js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// for (let i = 10; i >= 0; i--) {
+//   console.log(i);
+// }
+
+// let j = 0;
+
+// while (j < 10) {
+//   console.log(j);
+//   j++;
+// }
+
+console.log("Hello World");
+let username;
+
+do {
+  username = prompt("Wat is je naam?");
+} while (username === "");
